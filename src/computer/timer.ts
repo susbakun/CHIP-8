1c4e9b4b9ff684d39a338dd227e512879c052cbf
AmirSaeed AryanMehr <amiraryanmehr82@gmail.com>
2026-07-19 03:34:10 +0330

    beep beep

--- src/computer/timer.ts
+++ src/computer/timer.ts
@@ -1,13 +1,22 @@
+import player from "play-sound"
+
+
 export class Timer {
   public delayTimer = 0
   public soundTimer = 0
+  // used for beep sound
+  public player = player()
 
   public tick() {
     if (this.delayTimer > 0) this.delayTimer--
 
     if (this.soundTimer > 0) {
       this.soundTimer--
-      // TODO: splay beep sound
+
+      this.player.play('beep.mp3', (err) => {
+        console.error("failed to play audio", err)
+      })
+
     }
   }
 }
